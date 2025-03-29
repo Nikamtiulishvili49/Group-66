32742803d3baa2b854f9e41ffd4e9d48493fc581
nikoloz mtiulishvili <nikamtiula0@gmail.com>
2025-03-29 12:01:49 +0400

    second commit

---
+++ day 15/Homework/homework.py
@@ -0,0 +1,44 @@
+1 # გაიარეთ შემდეგი სამი სექცია Sololearn-ში:
+
+2 # გადახედეთ ჩანაწერს აუცილებლად, რადგან უკეთ გაიაზროთ ფუნქციები პითონში.
+
+3 # დარეგისტრირდით https://www.codewars.com-ზე.
+
+4 # მომხმარებელს შემოატანინეთ წინადადება. ტერმინალში კი დაბეჭდეთ ეს წინადადება ისე რომ იყოს:
+
+1. # ყველა პატარა ასო;
+2. # ყველა დიდი ასო;
+3. # პირველი ასო დიდი, ხოლო ყველა დანარჩენი პატარა.
+
+word = input("enter a word: ")
+
+print(word.lower())
+print(word.upper())
+print(word.capitalize())
+
+
+
+5 #  გააკეთეთ მეოთხე დავალება, თუმცა ამჯერად ჩააშენეთ კოდი ფუნქციაში.
+
+word = input("enter a word: ")
+
+print("All lowercase letters:" , word.lower())
+print("All uppercase letters: ", word.upper())
+print("First letter capitalized, the rest lowercase: ", word.capitalize())
+
+
+
+6 # გააკეთეთ შემდეგი Codewars-ები:
+
+# 6) https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/python
+
+# 7) https://www.codewars.com/kata/56200d610758762fb0000002/train/python
+
+# 8) https://www.codewars.com/kata/55685cd7ad70877c23000102/train/python
+
+
+
+
+
+
+
